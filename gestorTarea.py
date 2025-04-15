fad5a78b5754a79f3319dfaff34793992221358f
Julian Garcia <juliandavidgarciamoreno9@gmail.com>
2025-04-15 10:51:14 -0500

    mi primer commit

---
+++ gestorTarea.py
@@ -0,0 +1,22 @@
+import json
+def cargarJson(nombreArchivo):
+    try:
+        with open(nombreArchivo,"r",encoding="utf-8") as archivo:
+            contenido=archivo.read().strip()
+            if not contenido:
+                return {}
+            return json.loads(contenido)
+            
+    except FileNotFoundError:
+        return{}
+    except json.JSONDecodeError:
+        return{}
+    except PermissionError:
+        return{}
+    except IOError:
+        return{}
+    
+def actualizarJson(datos,nombreArchivo):
+    with open(nombreArchivo,"w", encoding="utf-8")as archivo:
+        json.dump(datos,archivo, indent=4)
+        
