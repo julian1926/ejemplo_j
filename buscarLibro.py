342fe5300fc6d1a0a55f27f930acdd5e5d56bc21
Julian Garcia <juliandavidgarciamoreno9@gmail.com>
2025-04-15 23:17:23 -0500

    vuevo titulo y un print vacio

---
+++ buscarLibro.py
@@ -0,0 +1,20 @@
+import gestor as g
+def buscarLibro():
+    libro_buscar=input("ingrese el titulo del libro que desea buscar: ")
+    libros=g.cargarJson("libros.json")
+    if not libros:
+        print("lista de libros vacia")
+        return
+    if libro_buscar in libros:
+        datos_libro=libros[libro_buscar]
+        print("----datos del libro-----")
+        print(f'Título: {libro_buscar}, Autor: {datos_libro.get("autor", "Desconocido")}, Cantidad: {datos_libro.get("cantidad", "N/A")}, Categoría: {datos_libro.get("categoria", "Sin categoría")} ')
+    else:
+        print(f"no se encontro ningun libro con el titulo {libro_buscar}")   
+
+
+
+
+
+    
+
